0ba2552a899681cb984f51552e675fc0aeb24716
Madhushan R.M.S <sadeepm20@gmail.com>
2024-10-13 14:19:36 +0530

    Update move_pose.cpp

--- smrr_executions/src/move_pose.cpp
+++ smrr_executions/src/move_pose.cpp
@@ -1,6 +1,7 @@
 #include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/pose_array.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <cmath>
 
 using std::placeholders::_1;
 
@@ -10,35 +11,49 @@ public:
   MovePoseNode() : Node("move_pose_node"),
                    move_group_interface(std::make_shared<rclcpp::Node>("moveit_node"), "arm")
   {
-    sub_ = create_subscription<geometry_msgs::msg::Pose>(
+    sub_ = create_subscription<geometry_msgs::msg::PoseArray>(
         "pose_topic", 5, std::bind(&MovePoseNode::msgCallback, this, _1));
 
     move_group_interface.setPlanningTime(5.0);
     move_group_interface.setPoseReferenceFrame("base_link");
     move_group_interface.setGoalPositionTolerance(0.001);
     move_group_interface.setGoalOrientationTolerance(3.14159);
+
+    target_poses.poses.resize(3);
   }
 
 private:
-  rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr sub_;
+  rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr sub_;
   moveit::planning_interface::MoveGroupInterface move_group_interface;
-  geometry_msgs::msg::Pose target_pose;
+  geometry_msgs::msg::Pose pose;
+  geometry_msgs::msg::PoseArray target_poses;
+  float threshold = 0.01;
   
-  void msgCallback(const geometry_msgs::msg::Pose &msg) 
-  {
-    if (msg.position.x != target_pose.position.x || msg.position.y != target_pose.position.y || msg.position.z != target_pose.position.z )
-    {
-        RCLCPP_INFO_STREAM(this->get_logger(), "New pose received");
+  void msgCallback(const geometry_msgs::msg::PoseArray &msg) 
+  {   
+    if ( 
+         (std::abs(msg.poses[0].position.x - target_poses.poses[0].position.x)) < threshold && 
+         (std::abs(msg.poses[0].position.y - target_poses.poses[0].position.y)) < threshold && 
+         (std::abs(msg.poses[0].position.z - target_poses.poses[0].position.z)) < threshold &&
 
-        target_pose.position.x = msg.position.x;
-        target_pose.position.y = msg.position.y;
-        target_pose.position.z = msg.position.z;
-        target_pose.orientation.x = msg.orientation.x;
-        target_pose.orientation.y = msg.orientation.y;
-        target_pose.orientation.z = msg.orientation.z;
+         (std::abs(msg.poses[1].position.x - target_poses.poses[1].position.x)) < threshold &&
+         (std::abs(msg.poses[1].position.y - target_poses.poses[1].position.y) )< threshold &&
+         (std::abs(msg.poses[1].position.z - target_poses.poses[1].position.z)) < threshold
+        )
+        
+    {   
+        RCLCPP_INFO_STREAM(this->get_logger(), "Same pose received. Ignoring ... ");
+        return;
+    }
+    
+    RCLCPP_INFO_STREAM(this->get_logger(), "New pose received. Moving the arm to the target poses");
+    target_poses = msg;
+
+    for (int i = 0; i < target_poses.poses.size(); i++)
+    {
 
         move_group_interface.setStartStateToCurrentState();
-        move_group_interface.setPoseTarget(target_pose);
+        move_group_interface.setPositionTarget(target_poses.poses[i].position.x, target_poses.poses[i].position.y, target_poses.poses[i].position.z, "eef_r_link" );
 
         // Create a plan to that target pose
         auto const [success, plan] = [&move_group_interface = move_group_interface]{
@@ -58,6 +73,10 @@ private:
             return;
         }
     }
+
+    std::vector<double> target_joint_values = {0.0, 0.0, 0.0, 0.0 };
+    move_group_interface.setJointValueTarget(target_joint_values);
+    move_group_interface.move();
   }
 };
 
