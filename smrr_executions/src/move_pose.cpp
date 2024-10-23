bea6d6df4362bee2e29b8cc704343774784dae5a
Madhushan R.M.S <sadeepm20@gmail.com>
2024-10-24 03:32:36 +0530

    Update move_pose.cpp

--- smrr_executions/src/move_pose.cpp
+++ smrr_executions/src/move_pose.cpp
@@ -2,58 +2,122 @@
 #include <geometry_msgs/msg/pose_array.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <cmath>
+#include <yaml-cpp/yaml.h>
+#include <fstream>
+#include <chrono>
 
 using std::placeholders::_1;
 
 class MovePoseNode : public rclcpp::Node
 {
 public:
-  MovePoseNode() : Node("move_pose_node"),
+  MovePoseNode() : Node("pose_to_angle_node"),
                    move_group_interface(std::make_shared<rclcpp::Node>("moveit_node"), "arm")
   {
-    sub_ = create_subscription<geometry_msgs::msg::PoseArray>(
-        "pose_topic", 5, std::bind(&MovePoseNode::msgCallback, this, _1));
+
+    this->declare_parameter<bool>("start_joint_calculations", false);
+
+    timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(100), 
+            std::bind(&MovePoseNode::timerCallback, this)) ;
+
+    yaml_path_ = "/home/sadeep/mobile_receptionist_ws/src/button_localization/config/elevator_interaction.yaml" ;
 
     move_group_interface.setPlanningTime(5.0);
     move_group_interface.setPoseReferenceFrame("base_link");
     move_group_interface.setGoalPositionTolerance(0.001);
     move_group_interface.setGoalOrientationTolerance(3.14159);
 
-    target_poses.poses.resize(3);
   }
 
 private:
-  rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr sub_;
   moveit::planning_interface::MoveGroupInterface move_group_interface;
-  geometry_msgs::msg::Pose pose;
-  geometry_msgs::msg::PoseArray target_poses;
-  float threshold = 0.01;
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::string yaml_path_ ;
   
-  void msgCallback(const geometry_msgs::msg::PoseArray &msg) 
-  {   
-    if ( 
-         (std::abs(msg.poses[0].position.x - target_poses.poses[0].position.x)) < threshold && 
-         (std::abs(msg.poses[0].position.y - target_poses.poses[0].position.y)) < threshold && 
-         (std::abs(msg.poses[0].position.z - target_poses.poses[0].position.z)) < threshold &&
 
-         (std::abs(msg.poses[1].position.x - target_poses.poses[1].position.x)) < threshold &&
-         (std::abs(msg.poses[1].position.y - target_poses.poses[1].position.y) )< threshold &&
-         (std::abs(msg.poses[1].position.z - target_poses.poses[1].position.z)) < threshold
-        )
-        
-    {   
-        RCLCPP_INFO_STREAM(this->get_logger(), "Same pose received. Ignoring ... ");
-        return;
-    }
-    
-    RCLCPP_INFO_STREAM(this->get_logger(), "New pose received. Moving the arm to the target poses");
-    target_poses = msg;
+  void timerCallback()
+  {
+    bool start_joint_calculations ;
+    this->get_parameter("start_joint_calculations", start_joint_calculations);
 
-    for (int i = 0; i < target_poses.poses.size(); i++)
+    if (start_joint_calculations)
     {
+      RCLCPP_INFO(rclcpp::get_logger("pose_to_angle"), "Starting to find target joint angles.");
+      bool ok1 = calculate_target_angles("initial_pose", "initial_joint_angles");
+      bool ok2 = calculate_target_angles("target_pose" , "target_joint_angles" );
+      bool ok3 = calculate_target_angles("initial_pose", "end_joint_angles"    );
+
+      if (ok1 && ok2)
+      {
+        RCLCPP_INFO(rclcpp::get_logger("pose_to_angle"), "Target joint angles are successfully updated.");
+      }
+      else
+      {
+        RCLCPP_ERROR(rclcpp::get_logger("pose_to_angle"), "Failed to find target joint angles.");
+
+      }
+
+      this->set_parameter(rclcpp::Parameter("start_joint_calculations", false));
+      this->set_parameter(rclcpp::Parameter("start_arm_control", true));
+      rclcpp::shutdown();
+      
+    }
+  }
+
+  bool calculate_target_angles(std::string pose, 
+                               std::string joint_angles_name)
+  {
+      YAML::Node data;
+    try {
+        data = YAML::LoadFile(this->yaml_path_);
+    } catch (const YAML::Exception& e) {
+        RCLCPP_ERROR(rclcpp::get_logger("pose_to_angle"), "Failed to load YAML file: %s", e.what());
+        rclcpp::shutdown();
+        return false;
+    }
+
+    if (!data["elevator_interaction"][pose]) {
+        RCLCPP_ERROR(rclcpp::get_logger("pose_to_angle"), "Pose %s not defined in YAML.", pose.c_str());
+        rclcpp::shutdown();
+        return false;
+    }
+
+      double x = data["elevator_interaction"][pose]["x"].as<double>();
+      double y = data["elevator_interaction"][pose]["y"].as<double>();
+      double z = data["elevator_interaction"][pose]["z"].as<double>();
+      RCLCPP_INFO(rclcpp::get_logger("done"), "done");
+
+      geometry_msgs::msg::Pose target_pose ;
+      target_pose.position.x  = x ;
+      target_pose.position.y  = y ;
+      target_pose.position.z  = z ;
+
+      const auto [success, target_joint_angles] = pose_to_angle(target_pose, move_group_interface);
+
+      if (success)
+      {
+        data["elevator_interaction"][joint_angles_name]["joint1"] = target_joint_angles[0];
+        data["elevator_interaction"][joint_angles_name]["joint2"] = target_joint_angles[1];
+        data["elevator_interaction"][joint_angles_name]["joint3"] = target_joint_angles[2];
+        data["elevator_interaction"][joint_angles_name]["joint4"] = target_joint_angles[3];
+        std::ofstream fout(this->yaml_path_);
+        fout << data;
+
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+  }
+
+  std::pair<bool, std::vector<double>> pose_to_angle(geometry_msgs::msg::Pose target_pose, 
+                    moveit::planning_interface::MoveGroupInterface& move_group_interface)
+  {
 
         move_group_interface.setStartStateToCurrentState();
-        move_group_interface.setPositionTarget(target_poses.poses[i].position.x, target_poses.poses[i].position.y, target_poses.poses[i].position.z, "eef_r_link" );
+        move_group_interface.setPositionTarget(target_pose.position.x, target_pose.position.y, target_pose.position.z, "eef_r_link" );
 
         // Create a plan to that target pose
         auto const [success, plan] = [&move_group_interface = move_group_interface]{
@@ -64,19 +128,17 @@ private:
 
         if (success)
         {
-            RCLCPP_INFO(this->get_logger(), "Planner SUCCEEDED, moving the arm and the gripper");
-            move_group_interface.execute(plan);
+            RCLCPP_INFO(rclcpp::get_logger("pose_to_angle"), "Planner succeeded, finding the target joint angles");
+            const auto final_waypoint = plan.trajectory_.joint_trajectory.points.back();
+            const std::vector<double> target_joint_angles = final_waypoint.positions;
+            return {true, target_joint_angles};
         }
         else
         {
-            RCLCPP_ERROR(this->get_logger(), "One or more planners failed!");
-            return;
+            RCLCPP_ERROR(rclcpp::get_logger("pose_to_angle"), "One or more planners failed!");
+            return{false, {}};
+            rclcpp::shutdown();
         }
-    }
-
-    std::vector<double> target_joint_values = {0.0, 0.0, 0.0, 0.0 };
-    move_group_interface.setJointValueTarget(target_joint_values);
-    move_group_interface.move();
   }
 };
 
