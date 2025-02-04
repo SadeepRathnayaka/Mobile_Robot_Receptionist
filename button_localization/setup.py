808699a860bd6c64d9378c943091ccb894bf1a94
Madhushan R.M.S <sadeepm20@gmail.com>
2025-02-04 20:44:20 +0530

    Update setup.py

--- button_localization/setup.py
+++ button_localization/setup.py
@@ -12,8 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/visualization.launch.py')),
-        (os.path.join('share', package_name, 'launch'), glob('launch/button_localization.launch.py'))
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
